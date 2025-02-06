b562c0435d21540db29a240c8f96d8514bf6d896
HengzhiShao-ANZ <hengzhi.shao@anz.com>
2025-02-06 15:33:03 +1100

    Update arithmetics.go

--- internal/challenge/implme/basic/arithmetics/arithmetics.go
+++ internal/challenge/implme/basic/arithmetics/arithmetics.go
@@ -1,7 +1,13 @@
 package arithmetics
 
 import (
+	"context"
+	"log/slog"
+	"sync"
+	"sync/atomic"
 	"time"
+
+	"github.com/romangurevitch/concurrencyworkshop/internal/pattern/fanoutin"
 )
 
 func SequentialSum(inputSize int) int {
@@ -12,12 +18,51 @@ func SequentialSum(inputSize int) int {
 	return sum
 }
 
+// Example squareNonNegative function that squares non-negative integer.
+func squareNonNegative(_ context.Context, value int) (int, error) {
+	return process(value), nil
+}
+
 // ParallelSum implement this method.
-func ParallelSum(_ int) int {
-	panic("implement me!")
+func ParallelSum(inputSize int) int {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	var sum atomic.Int64
+	var jobs []fanoutin.Job[int]
+	for i := 1; i <= inputSize; i++ {
+		jobs = append(jobs, fanoutin.Job[int]{ID: i, Value: i})
+	}
+
+	// Fan out
+	results := fanoutin.FanOut(ctx, jobs, squareNonNegative)
+
+	wg := sync.WaitGroup{}
+	// Fan in
+	for result := range results {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				if result.Err != nil {
+					slog.Error("Error processing job", "jobID", result.Job.ID, "error", result.Err)
+					cancel()
+					return
+				}
+				slog.Info("Result for job", "jobID", result.Job.ID, "result", result.Value)
+				sum.Add(int64(result.Value))
+			}
+		}()
+	}
+
+	wg.Wait()
+	return int(sum.Load())
 }
 
 func process(num int) int {
-	time.Sleep(time.Millisecond) // simulate processing time
+	time.Sleep(100 * time.Millisecond) // simulate processing time
 	return num * num
 }
